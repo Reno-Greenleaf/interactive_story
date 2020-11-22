04fc4b789b143ffc3e09ff6a12a35bb7e8f3851d
Reno Greenleaf <greenleaf@tut.by>
2020-11-22 20:32:49 +0300

    Commands belong to a game.

--- command/models.py
+++ command/models.py
@@ -1,6 +1,8 @@
 from django.db import models
+from game.models import Game
 
 
 class Command(models.Model):
     text = models.CharField(max_length=300, blank=False, null=False)
     output = models.TextField(default='')
+    game = models.ForeignKey(Game, on_delete=models.CASCADE, related_name='commands')
--- command/views.py
+++ command/views.py
@@ -4,13 +4,14 @@ from django.http import HttpResponseRedirect
 from django.urls import reverse
 from django.contrib import messages
 from game.views import GameView
+from game.models import Game
 from command.models import Command
 from command.forms import CommandForm
 
 
 class AddCommand(GameView):
     def get(self, request):
-        commands = Command.objects.all()
+        commands = self.current_game.commands.all()
         form = CommandForm()
         return render(request, 'command/add-command.html', {'commands': commands, 'form': form})
 
@@ -18,26 +19,26 @@ class AddCommand(GameView):
         form = CommandForm(request.POST)
 
         if not form.is_valid():
-            commands = Command.objects.all()
+            commands = self.current_game.commands.all()
             return render(request, 'command/add-command.html', {'commands': commands, 'form': form})
 
-        Command.objects.create(text=form.cleaned_data['text'], output=form.cleaned_data['output'])
+        self.current_game.commands.create(text=form.cleaned_data['text'], output=form.cleaned_data['output'])
         return HttpResponseRedirect(reverse('add-command'))
 
 
 class EditCommand(GameView):
     def get(self, request, command_id):
-        commands = Command.objects.all()
-        command = Command.objects.get(pk=command_id)
+        commands = self.current_game.commands.all()
+        command = self.current_game.commands.get(pk=command_id)
         form = CommandForm(initial={'text': command.text, 'output': command.output})
         return render(request, 'command/edit-command.html', {'commands': commands, 'command': command, 'form': form})
 
     def post(self, request, command_id):
         form = CommandForm(request.POST)
-        command = Command.objects.get(pk=command_id)
+        command = self.current_game.commands.get(pk=command_id)
 
         if not form.is_valid():
-            commands = Command.objects.all()
+            commands = self.current_game.commands.all()
             return render(request, 'command/edit-command.html', {'commands': commands, 'command': command, 'form': form})
 
         command.text = form.cleaned_data['text']
@@ -48,11 +49,11 @@ class EditCommand(GameView):
 
 class DeleteCommand(GameView):
     def get(self, request, command_id):
-        command = Command.objects.get(pk=command_id)
+        command = self.current_game.commands.get(pk=command_id)
         return render(request, 'command/delete-command.html', {'command': command})
 
     def post(self, request, command_id):
-        command = Command.objects.get(pk=command_id)
+        command = self.current_game.commands.get(pk=command_id)
         command.delete()
         messages.add_message(request, messages.INFO, 'Removed "{0}"'.format(command.text))
         return HttpResponseRedirect(reverse('add-command'))
