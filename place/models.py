7d2a5128eb6fe5e9d7c29c16fdecff6c11d149c8
Reno Greenleaf <greenleaf@tut.by>
2020-11-22 20:55:49 +0300

    Places belong to a game.

--- place/models.py
+++ place/models.py
@@ -1,6 +1,8 @@
 from django.db import models
+from game.models import Game
 
 
 class Place(models.Model):
     name = models.CharField(max_length=300, blank=False, null=False)
     description = models.TextField(default='')
+    game = models.ForeignKey(Game, on_delete=models.CASCADE, related_name='places')
--- place/views.py
+++ place/views.py
@@ -11,33 +11,33 @@ from place.forms import PlaceForm
 class AddPlace(GameView):
     def get(self, request):
         form = PlaceForm()
-        places = Place.objects.all()
+        places = self.current_game.places.all()
         return render(request, 'place/add-place.html', {'form': form, 'places': places})
 
     def post(self, request):
         form = PlaceForm(request.POST)
 
         if not form.is_valid():
-            places = Place.objects.all()
+            places = self.current_game.places.all()
             return render(request, 'place/add-place.html', {'form': form, 'places': places})
 
-        Place.objects.create(name=form.cleaned_data['name'], description=form.cleaned_data['description'])
+        self.current_game.places.create(name=form.cleaned_data['name'], description=form.cleaned_data['description'])
         return HttpResponseRedirect(reverse('add-place'))
 
 
 class EditPlace(GameView):
     def get(self, request, place_id):
-        places = Place.objects.all()
-        place = Place.objects.get(pk=place_id)
+        places = self.current_game.places.all()
+        place = self.current_game.places.get(pk=place_id)
         form = PlaceForm(initial={'name': place.name, 'description': place.description})
         return render(request, 'place/edit-place.html', {'places': places, 'place': place, 'form': form})
 
     def post(self, request, place_id):
         form = PlaceForm(request.POST)
-        place = Place.objects.get(pk=place_id)
+        place = self.current_game.places.get(pk=place_id)
 
         if not form.is_valid():
-            places = Place.objects.all()
+            places = self.current_game.places.all()
             return render(request, 'place/edit-place.html', {'places': places, 'place': place, 'form': form})
 
         place.name = form.cleaned_data['name']
@@ -48,11 +48,11 @@ class EditPlace(GameView):
 
 class DeletePlace(GameView):
     def get(self, request, place_id):
-        place = Place.objects.get(pk=place_id)
+        place = self.current_game.places.get(pk=place_id)
         return render(request, 'place/delete-place.html', {'place': place})
 
     def post(self, request, place_id):
-        place = Place.objects.get(pk=place_id)
+        place = self.current_game.places.get(pk=place_id)
         place.delete()
         messages.add_message(request, messages.INFO, 'Removed "{0}"'.format(place.name))
         return HttpResponseRedirect(reverse('add-place'))
