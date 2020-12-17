1bc22771e36835351b90fc568486438276111e36
Reno Greenleaf <greenleaf@tut.by>
2020-12-17 17:44:36 +0300

    Logout.

--- custom_user/views.py
+++ custom_user/views.py
@@ -3,7 +3,7 @@ from django.contrib import messages
 from django.views import View
 from django.http import HttpResponseRedirect
 from django.urls import reverse
-from django.contrib.auth import authenticate, login
+from django.contrib.auth import authenticate, login, logout
 
 from custom_user.forms import LoginForm
 
@@ -29,3 +29,9 @@ class Login(View):
 
         messages.add_message(request, messages.INFO, 'Credentials are invalid')
         return render(request, 'custom_user/login.html', {'form': form})
+
+
+class Logout(View):
+    def get(self, request):
+        logout(request)
+        return HttpResponseRedirect(reverse('login'))
--- interactive_story/urls.py
+++ interactive_story/urls.py
@@ -21,7 +21,7 @@ from command.views import AddCommand, EditCommand, DeleteCommand
 from game.views import CreateGame, DeleteGame, EditGame, SelectGame, Games
 from place.views import AddPlace, EditPlace, DeletePlace
 from player.views import Play, Start, Continue
-from custom_user.views import Login
+from custom_user.views import Login, Logout
 
 urlpatterns = [
     path('', Games.as_view(), name='games'),
@@ -41,4 +41,5 @@ urlpatterns = [
     path('start/<int:game_id>', Start.as_view(), name='start'),
     path('continue/<int:session_id>', Continue.as_view(), name='continue'),
     path('login', Login.as_view(), name='login'),
+    path('logout', Logout.as_view(), name='logout'),
 ]
